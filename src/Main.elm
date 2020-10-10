8f6d614d446e066641bb8eaf3e3e06d11364a19f
Rasmus Vermeulen <rasmus_vermeulen@tutanota.com>
2020-10-10 14:10:27 +0200

    Use elm-ui over elm/html

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,13 @@
 module Main exposing (..)
 
 import Browser
-import Html exposing (Html, text, div, h1, img)
-import Html.Attributes exposing (src)
+import Element exposing (..)
+import Element.Background as Background
+import Element.Border as Border
+import Element.Font as Font
+import Maybe
+import Set exposing (Set)
+
 
 
 ---- MODEL ----
@@ -34,22 +39,32 @@ update msg model =
 ---- VIEW ----
 
 
-view : Model -> Html Msg
+view : Model -> Element Msg
 view model =
-    div []
-        [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
+    column [ padding 10, spacing 20, Background.color (rgb 0.2 0.2 0.2) ]
+        [ el [ Font.color (rgb 0.7 0.7 1) ] <| text "this"
+        , el [ Font.color (rgb 1 1 0.7) ] <| text "is a"
+        , el [ Font.color (rgb 1 0.7 1) ] <| text "column"
+        , row [ spacing 20, padding 10, Background.color (rgb 0 0 0) ]
+            [ el [ Font.color (rgb 1 1 0.7) ] <| text "and this"
+            , el [ Font.color (rgb 1 0.7 1) ] <| text "is a row"
+            ]
         ]
 
 
 
 ---- PROGRAM ----
+-- state serialized as json
 
 
-main : Program () Model Msg
+type alias Flags =
+    ()
+
+
+main : Program Flags Model Msg
 main =
     Browser.element
-        { view = view
+        { view = \model -> layout [] (view model)
         , init = \_ -> init
         , update = update
         , subscriptions = always Sub.none
