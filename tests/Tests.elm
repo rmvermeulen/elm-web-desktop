084fedca5b4c543b8e68995cee462bbd40894b29
Rasmus Vermeulen <rasmus_vermeulen@tutanota.com>
2020-12-06 21:01:48 +0100

    tests: single file for now

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -1,20 +1,106 @@
 module Tests exposing (..)
 
 import Expect
+import Store
 import Test exposing (..)
 
 
-
--- Check out https://package.elm-lang.org/packages/elm-explorations/test/latest to learn more about testing in Elm!
-
-
-all : Test
-all =
-    describe "A Test Suite"
-        [ test "Addition" <|
+storeModule : Test
+storeModule =
+    describe "Store module"
+        [ test "Store.empty" <|
             \_ ->
-                Expect.equal 10 (3 + 7)
-        , test "String.left" <|
+                Expect.equal (Store.size Store.empty) 0
+        , test "Store.fromList" <|
             \_ ->
-                Expect.equal "a" (String.left 1 "abcdefg")
+                let
+                    list =
+                        [ 10, 10, 2, 5, 10 ]
+
+                    store =
+                        Store.fromList list
+                in
+                Expect.equal (Store.size store) (List.length list)
+        , test "Store.add" <|
+            \_ ->
+                let
+                    ( Store.Id id, store ) =
+                        Store.add "Some value" Store.empty
+                in
+                Expect.equal (Store.size store) (id + 1)
+        , test "Store.member (empty)" <|
+            \_ ->
+                let
+                    ( id, _ ) =
+                        Store.add "Some value" Store.empty
+                in
+                Expect.false "Id in empty store" (Store.member id Store.empty)
+        , test "Store.member" <|
+            \_ ->
+                let
+                    ( id, store ) =
+                        Store.add "Some value" Store.empty
+                in
+                Expect.true ("Id not in store " ++ Debug.toString store ++ " " ++ Debug.toString id) (Store.member id store)
+        , test "Store.replace" <|
+            \_ ->
+                let
+                    ( id, store ) =
+                        Store.add "Some value" Store.empty
+
+                    newStore =
+                        Store.replace id "Different value" store
+                in
+                if Store.member id store then
+                    Expect.equal (Store.values newStore) [ "Different value" ]
+
+                else
+                    Expect.fail "Could not replace id"
+        , test "Store.mapSingle" <|
+            \_ ->
+                let
+                    ( id, store ) =
+                        Store.add "Some value" Store.empty
+
+                    newStore =
+                        Store.mapSingle id (\_ -> "Different value") store
+                in
+                if Store.member id store then
+                    Expect.equal (Store.values newStore) [ "Different value" ]
+
+                else
+                    Expect.fail "Could not mapSingle at id"
+        , test "Store.values" <|
+            \_ ->
+                let
+                    list =
+                        [ 10, 10, 2, 5, 10 ]
+
+                    store =
+                        Store.fromList list
+                in
+                Expect.equal (Store.values store) list
+        , test "Store.pairs" <|
+            \_ ->
+                let
+                    list =
+                        [ 10, 10, 2, 5, 10 ]
+
+                    store =
+                        Store.fromList list
+
+                    expected =
+                        list |> List.indexedMap (\i item -> ( Store.Id i, item ))
+                in
+                Expect.equal (Store.pairs store) expected
+        , test "Store.map" <|
+            \_ ->
+                let
+                    ( _, store ) =
+                        Store.add "Some value" Store.empty
+
+                    fn k v =
+                        v ++ ", and another value"
+                in
+                Expect.equal (Store.map fn store |> Store.values) [ "Some value, and another value" ]
         ]
